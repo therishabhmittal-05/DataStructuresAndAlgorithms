8b6a4c3000140c425cd25cb71564389831ebd218
Rishabh Mittal <122997682+therishabhmittal-05@users.noreply.github.com>
2023-08-30 07:42:50 +0000

    change

--- Basics/sumEvenAndOdd.cpp
+++ Basics/sumEvenAndOdd.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   int N;
-   cin >> N; 
-   int sumEven = 0;
-   int sumOdd = 0;
-   for (int i=0; N!=0 ; i++){
-       int x = N%10;
-       if (x%2 == 0){
-           sumEven = sumEven + x;
-           }
-       
-        else {
-                sumOdd = sumOdd + x;
-            }
-        N = N/10;
-       }
-    cout << sumEven << "\t" << sumOdd;
-   
+int main()
+{
+    int N;
+    cin >> N;
+    int sumEven = 0;
+    int sumOdd = 0;
+
+    while (N != 0)
+    {
+        int x = N % 10;
+        if (x % 2 == 0)
+        {
+            sumEven = sumEven + x;
+        }
+
+        else
+        {
+            sumOdd = sumOdd + x;
+        }
+        N = N / 10;
+    }
+    cout << sumEven << " " << sumOdd;
+
     return 0;
 }
